9d6336486f30397cf4ab9475c8e06bebfd43c82b
SanketGade07 <sjgade72@gmail.com>
2025-06-18 18:22:36 +0530

    Initial backend setup: Express and dotenv configured

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Movie Booking API is running...');
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
