2613956b0f6068609a7ef40b997e96a2a1119aef
SanketGade07 <sjgade72@gmail.com>
2025-07-03 14:19:15 +0530

    refactor: update show.model.js and theatre.model.js models also change name of  seed.js to seedMovies.js

--- backend/models/show.model.js
+++ backend/models/show.model.js
@@ -1,10 +1,10 @@
+
 const mongoose = require('mongoose');
 
-const showSchema = new mongoose.Schema({
-  movie_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
-  screen_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Screen' },
-  show_time: Date,
-  price_per_seat: Number
-});
+const ShowtimeSchema = new mongoose.Schema({
+    theaterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Theater', required: true },
+    time: { type: String, required: true }, // e.g., "10:00 AM"
+    screenType: { type: String, enum: ["IMAX"], default: "IMAX" },
+}, { timestamps: true });
 
-module.exports = mongoose.model('Show', showSchema);
+module.exports = mongoose.model('Showtime', ShowtimeSchema);
--- backend/models/theatre.model.js
+++ backend/models/theatre.model.js
@@ -1,9 +1,12 @@
+// models/Theater.js
 const mongoose = require('mongoose');
 
-const theatreSchema = new mongoose.Schema({
-  name: String,
-  city: String,
-  address: String
-});
+const TheaterSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    chain: { type: String, enum: ["Cinepolis", "INOX", "PVR ICON"], required: true },
+    facilities: [{ type: String, enum: ["M-Ticket", "Food & Beverage"] }],
+    cancellable: { type: Boolean, required: true },
+}, { timestamps: true });
 
-module.exports = mongoose.model('Theatre', theatreSchema);
+module.exports = mongoose.model('Theater', TheaterSchema);
